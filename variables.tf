ba7acee0bac076462aaf312ebb6a6ff7269f77b5
adeola2020-git <aadefolaju@gmail.com>
2023-03-24 05:33:12 +0000

    added config files

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "azs" {
+  type    = list(string)
+  default = ["eu-west-2a", "eu-west-2b"]
+}
+
+variable "ami" {
+  default = "ami-038d76c4d28805c09" #(ubuntu ami)
+}
+
+
+
+
